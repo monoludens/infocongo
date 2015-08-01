da98be422502e924d89683659d3a6263fdc8da3a
monoludens <wil.cavalheiro@gmail.com>
2015-07-31 21:12:06 -0300

    header and footer init

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+	</div><!-- .site-content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+		</div><!-- .site-info -->
+	</footer><!-- .site-footer -->
+</div> <!-- .container -->
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="UTF-8">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<div class="container">
+		<div id="content" class="site-content">
