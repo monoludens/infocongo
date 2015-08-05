f390aac86173aabec6fe7a7098c80fd40df5a483
monoludens <wil.cavalheiro@gmail.com>
2015-08-04 23:28:38 -0300

    home html init

--- index.php
+++ index.php
@@ -1,6 +1,36 @@
 <?php get_header(); ?>
 
 
+<div id="slider">
+	<div class="container">
+		<div class="row">
+			<div class="six columns"></div>
+			<div class="six columns slider-logo"><img src="" alt=""></div>
+			<div class="six columns"></div>
+		</div>
+		<div class="row">
+			<div class="six columns"></div>
+			<div class="six columns slider-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod </div>
+			<div class="six columns"></div>
+		</div>
+	</div>
+</div>
 
+<div id="featured-content">
+	<div class="content">
+		<div class="row">
+			<div class="six columns"><img src="" alt=""></div>
+			<div class="one column"></div>
+			<div class="five columns">
+				<h2>story title</h2>
+				<div><i></i><p class=""><b>author:</b></p></div>
+				<div><i></i><p class=""><b>place:</b></p></div>
+				<div><i></i><p class=""><b>topics:</b></p></div>
+				<p class="excerpt"></p>
+				<button></button>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php get_footer(); ?>
