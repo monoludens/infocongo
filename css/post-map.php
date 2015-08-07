75e07d9abd8dd084bf16ae21d83fcefa6d9cca6e
monoludens <wil.cavalheiro@gmail.com>
2015-08-07 16:31:25 -0300

    post map init

---
+++ css/post-map.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<div class="container">
+		<div class="ten offset-by-one columns">
+			<h1>	</h1>
+			<p></p>
+			<div class="meta-icons">
+				<span class="icon"></span>
+				<span></span>
+				<span></span>
+			</div>
+			<div class="meta-icons">
+				<span class="icon"></span>
+				<span></span>
+				<span></span>
+			</div>
+			<div class="meta-icons">
+				<span class="icon"></span>
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+	</div>	
+
+<?php get_footer(); ?>
