3a3c70b306fa41826f178e9b832e00eb5f9e411e
monoludens <wil.cavalheiro@gmail.com>
2015-08-07 16:39:09 -0300

    post map rename realocate

---
+++ single.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+<div class="container">
+	<div class="ten offset-by-one columns">
+		<h1>	</h1>
+		<p></p>
+		<div class="meta-icons">
+			<span class="icon"></span>
+			<span></span>
+			<span></span>
+		</div>
+		<div class="meta-icons">
+			<span class="icon"></span>
+			<span></span>
+			<span></span>
+		</div>
+		<div class="meta-icons">
+			<span class="icon"></span>
+			<span></span>
+			<span></span>
+		</div>
+	</div>
+</div>	
+
+<div class="map-container"><div id="map_<?php echo jeo_get_map_id(); ?>" class="map"></div></div>
+
+<?php get_footer(); ?>
