d299f41ddd69db86bf3cf0bf85b2e1404bb21214
monoludens <wil.cavalheiro@gmail.com>
2015-08-08 12:38:43 -0300

    custom taxonomies add (topics and countries)

--- functions.php
+++ functions.php
@@ -103,3 +103,70 @@ function images_theme_setup() {
   add_image_size( 'home-list', 300, 200, array( 'center', 'top' ));
   add_image_size( 'home-slider', 540, 200, array( 'center', 'top' ));
 }
+
+
+// Register Custom Taxonomy
+function custom_taxonomy() {
+
+  $labels = array(
+    'name'                       => _x( 'Topics', 'Taxonomy General Name', 'text_domain' ),
+    'singular_name'              => _x( 'Topic', 'Taxonomy Singular Name', 'text_domain' ),
+    'menu_name'                  => __( 'Topics', 'text_domain' ),
+    'all_items'                  => __( 'All Topics', 'text_domain' ),
+    'parent_item'                => __( 'Parent Topics', 'text_domain' ),
+    'parent_item_colon'          => __( 'Parent Topic:', 'text_domain' ),
+    'new_item_name'              => __( 'New Topic Name', 'text_domain' ),
+    'add_new_item'               => __( 'Add New Topic', 'text_domain' ),
+    'edit_item'                  => __( 'Edit Topic', 'text_domain' ),
+    'update_item'                => __( 'Update Topic', 'text_domain' ),
+    'view_item'                  => __( 'View Topic', 'text_domain' ),
+    'separate_items_with_commas' => __( 'Separate Topic with commas', 'text_domain' ),
+    'add_or_remove_items'        => __( 'Add or remove Topic', 'text_domain' ),
+    'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+    'popular_items'              => __( 'Popular Topics', 'text_domain' ),
+    'search_items'               => __( 'Search Topics', 'text_domain' ),
+    'not_found'                  => __( 'Not Found', 'text_domain' ),
+  );
+  $args = array(
+    'labels'                     => $labels,
+    'hierarchical'               => true,
+    'public'                     => true,
+    'show_ui'                    => true,
+    'show_admin_column'          => true,
+    'show_in_nav_menus'          => true,
+    'show_tagcloud'              => true,
+  );
+  register_taxonomy( 'Topic', array( 'post' ), $args );
+
+  $labels = array(
+    'name'                       => _x( 'Countries', 'Taxonomy General Name', 'text_domain' ),
+    'singular_name'              => _x( 'Country', 'Taxonomy Singular Name', 'text_domain' ),
+    'menu_name'                  => __( 'Countries', 'text_domain' ),
+    'all_items'                  => __( 'All Countries', 'text_domain' ),
+    'parent_item'                => __( 'Parent Countries', 'text_domain' ),
+    'parent_item_colon'          => __( 'Parent Country:', 'text_domain' ),
+    'new_item_name'              => __( 'New Country Name', 'text_domain' ),
+    'add_new_item'               => __( 'Add New Country', 'text_domain' ),
+    'edit_item'                  => __( 'Edit Country', 'text_domain' ),
+    'update_item'                => __( 'Update Country', 'text_domain' ),
+    'view_item'                  => __( 'View Country', 'text_domain' ),
+    'separate_items_with_commas' => __( 'Separate Country with commas', 'text_domain' ),
+    'add_or_remove_items'        => __( 'Add or remove Country', 'text_domain' ),
+    'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+    'popular_items'              => __( 'Popular Countries', 'text_domain' ),
+    'search_items'               => __( 'Search Countries', 'text_domain' ),
+    'not_found'                  => __( 'Not Found', 'text_domain' ),
+  );
+  $args = array(
+    'labels'                     => $labels,
+    'hierarchical'               => true,
+    'public'                     => true,
+    'show_ui'                    => true,
+    'show_admin_column'          => true,
+    'show_in_nav_menus'          => true,
+    'show_tagcloud'              => true,
+  );
+  register_taxonomy( 'Country', array( 'post' ), $args );
+
+}
+add_action( 'init', 'custom_taxonomy', 0 );
