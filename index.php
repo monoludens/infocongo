6bae1d7214521426f97727b10beca46d2002e50d
monoludens <wil.cavalheiro@gmail.com>
2015-08-18 13:46:47 -0300

    jquery topics and countries filter fixes

--- index.php
+++ index.php
@@ -1,4 +1,61 @@
 <?php get_header(); ?>
+<script>
+	$.noConflict();
+	jQuery( document ).ready(function( $ ) {
+
+		var $topic_list = $('#topic-list li');
+		var $topic_content = $('.topic-content li');
+		var $topic_list_first = $('#topic-list li:first-child');
+		var $topic_a = $topic_list_first.attr('id');
+
+		$topic_list_first.addClass('active');
+		$topic_content.each(function() {
+			if ($(this).attr('id').indexOf($topic_a)) {
+				$(this).addClass('active');
+			}
+		});
+
+		$topic_list.click(function() {
+
+			$topic_list.removeClass('active');
+			$topic_content.removeClass('active');
+
+			var $topic_b = $(this).attr('id');
+			$(this).addClass('active');
+			$topic_content.each(function() {
+				if ($(this).attr('id').indexOf($topic_b)) {
+					$(this).addClass('active');
+				}
+			});
+		});
+
+		var $country_list = $('#country-list li');
+		var $country_content = $('.country-content li');
+		var $country_list_first = $('#country-list li:first-child');
+		var $country_a = $country_list_first.attr('id');
+
+		$country_list_first.addClass('active');
+		$country_content.each(function() {
+			if ($(this).attr('id').indexOf($country_a)) {
+				$(this).addClass('active');
+			}
+		});
+
+		$country_list.click(function() {
+
+			$country_list.removeClass('active');
+			$country_content.removeClass('active');
+
+			var $country_b = $(this).attr('id');
+			$(this).addClass('active');
+			$country_content.each(function() {
+				if ($(this).attr('id').indexOf($country_b)) {
+					$(this).addClass('active');
+				}
+			});
+		});
+	});
+</script>
 
 
 <div id="intro" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/examples/intro-header.png)">
@@ -11,6 +68,8 @@
 		</div>
 	</div>
 </div>
+
+<!-- Featured -->
 <?php $first_query = new WP_Query('cat=Featured&posts_per_page=1');
 	while($first_query->have_posts()) : $first_query->the_post();
 ?>
@@ -39,125 +98,97 @@
 <?php endwhile; ?>
 <?php wp_reset_postdata(); ?>
 
-<?php $second_query = new WP_Query( array( 'taxonomy' => 'topic', 'term' => 'Deforestation', 'posts_per_page' => 4)); ?>
-<div class="list-content">
-	<div class="container">
-		<div class="three columns">
-			<h2><?php _e( 'Topics' ) ?></h2>
-			<?php 
-			    $args = array(
-				'show_option_all'    => '',
-				'orderby'            => 'name',
-				'order'              => 'ASC',
-				'style'              => 'list',
-				'hide_empty'         => 1,
-				'use_desc_for_title' => 1,
-				'title_li'           => __( '' ),
-				'number'             => null,
-				'echo'               => 1,
-				'depth'              => 0,
-				'current_category'   => 1,
-				'pad_counts'         => 0,
-				'taxonomy'           => 'topic',
-				'walker'             => null
-			    );
-			    wp_list_categories( $args ); 
-			?>
-					</div>
-		<div class="nine columns">
-			<ul>
-			<?php while($second_query->have_posts()) : $second_query->the_post(); ?>
-				<li class="home-post-list">
-					<?php if(has_post_thumbnail()) {                    
-					    $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(), 'home-list' );
-					     echo '<img src="' . $image_src[0]  . '" width="100%"  />';
-					} ?>
-					<h6> <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
-					<div class="post-list-icon">
-						<span class="icon_pin_alt"></span><span><?php echo get_the_term_list( $post->ID, 'country', ' ', ', ' ); ?></span>
-					</div>
-				</li>
-				<?php endwhile; ?>
-			</ul>
-		</div>
-	</div>
-</div>
-
-
-
-
-
-
-
-
-<?php $third_query = new WP_Query( array( 'taxonomy' => 'country')); ?>
-<div class="list-content">
-	<div class="container">
-		<div class="three columns">
-			<h2><?php _e( 'Countries', 'infocongo' ) ?></h2>
-			<ul id="topic-list">
-				<?php
-				$terms = get_terms( 'country' );
-				 if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
-				     foreach ( $terms as $term ) {
-				       echo '<li id="' . $term->slug . '">' . $term->name . '</li>';
-				     }
-				 }
-				 ?>
-			</ul>
-		</div>
-		<div class="nine columns">
-			<div class="topic-content">
-				<ul>
-				<?php while($third_query->have_posts()) : $third_query->the_post(); ?>
-					<?php $tax = 'country'; ?>
-						<?php $tax_post = wp_get_object_terms( $post->ID, $tax ); ?>
-						<li id="<?php foreach($tax_post as $slug) { 
-								echo $slug->slug . " ";	
-							}?>" class="home-post-list">
-							
-								<?php if(has_post_thumbnail()) {                    
-								    $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(), 'home-list' );
-								     echo '<img src="' . $image_src[0]  . '" width="100%"  />';
-								} ?>
-								<h6><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
-								<span class="icon_pin_alt"></span><span><?php echo get_the_term_list( $post->ID, 'country', ' ', ', ' ); ?></span>
-							</li>					
-				<?php endwhile; ?>
+<!-- Topics -->
+<?php $second_query = new WP_Query( array( 'taxonomy' => 'topic')); ?>
+	<div class="list-content">
+		<div class="container">
+			<div class="three columns">
+				<h2><?php _e( 'Topics', 'infocongo' ) ?></h2>
+				<ul id="topic-list">
+					<?php
+					$terms = get_terms( 'topic' );
+					 if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+					     foreach ( $terms as $term ) {
+					       echo '<li id="' . $term->slug . '">' . $term->name . '</li>';
+					     }
+					 }
+					 ?>
 				</ul>
-				<a href="<?php foreach($tax_post as $slug) { 
-								echo $slug->slug . " ";	
-							}?>" class="button"> <?php _e('See all stories about this topic', 'infocongo'); ?></a>
 			</div>
-					
-				<script>
-	
-					  $(document).ready(function() {
-
-					  var $list = $('#topic-list li');
-					  var $content = $('.topic-content li');
-
-					  $list.click(function() {
-
-					      $list.removeClass('active');
-					      $content.removeClass('active');
-
-					      var $c = $(this).attr('id');
-					      $(this).addClass('active');
-					      $content.each(function() {
-					          if ($(this).attr('id').indexOf($c)) {
-					              $(this).addClass('active');
-					          }
-					      });
-
-					  });
-					});
-				</script>
+			<div class="nine columns">
+				<div class="topic-content">
+					<ul>
+						<?php while($second_query->have_posts()) : $second_query->the_post(); ?>
+							<?php $tax = 'topic'; ?>
+								<?php $tax_post = wp_get_object_terms( $post->ID, $tax ); ?>
+								<li id="<?php foreach($tax_post as $slug) { 
+										echo $slug->slug . " ";	
+									}?>" class="home-post-list">
+									
+										<?php if(has_post_thumbnail()) {                    
+										    $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(), 'home-list' );
+										     echo '<img src="' . $image_src[0]  . '" width="100%"  />';
+										} ?>
+										<h6><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
+										<span class="icon_pin_alt"></span><span><?php echo get_the_term_list( $post->ID, 'country', ' ', ', ' ); ?></span>
+									</li>					
+						<?php endwhile; ?>
+					</ul>
+					<!--<a href="<?php foreach($tax_post as $slug) { 
+							echo $slug->slug . " ";	
+						}?>" class="button"> <?php _e('See all stories about this topic', 'infocongo'); ?>
+					</a>-->
+				</div>
+			</div>
 		</div>
 	</div>
-</div>
-
 <?php wp_reset_postdata(); ?>
+<!-- Country -->
+<?php $third_query = new WP_Query( array( 'taxonomy' => 'country')); ?>
+	<div class="list-content">
+		<div class="container">
+			<div class="three columns">
+				<h2><?php _e( 'Countries', 'infocongo' ) ?></h2>
+				<ul id="country-list">
+					<?php
+					$terms = get_terms( 'country' );
+					 if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+					     foreach ( $terms as $term ) {
+					       echo '<li id="' . $term->slug . '">' . $term->name . '</li>';
+					     }
+					 }
+					 ?>
+				</ul>
+			</div>
+			<div class="nine columns">
+				<div class="country-content">
+					<ul>
+						<?php while($third_query->have_posts()) : $third_query->the_post(); ?>
+							<?php $tax = 'country'; ?>
+								<?php $tax_post = wp_get_object_terms( $post->ID, $tax ); ?>
+								<li id="<?php foreach($tax_post as $slug) { 
+										echo $slug->slug . " ";	
+									}?>" class="home-post-list">
+									
+										<?php if(has_post_thumbnail()) {                    
+										    $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(), 'home-list' );
+										     echo '<img src="' . $image_src[0]  . '" width="100%"  />';
+										} ?>
+										<h6><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
+										<span class="icon_pin_alt"></span><span><?php echo get_the_term_list( $post->ID, 'country', ' ', ', ' ); ?></span>
+									</li>					
+						<?php endwhile; ?>
+					</ul>
+					<!--<a href="<?php foreach($tax_post as $slug) { 
+							echo $slug->slug . " ";	
+						}?>" class="button"> <?php _e('See all stories about this topic', 'infocongo'); ?>
+					</a>-->
+				</div>
+			</div>
+		</div>
+	</div>
+<?php wp_reset_postdata(); ?>
+
 <?php
 	$args = array(
 	    //'meta_key' => 'post_views_count',
@@ -167,6 +198,7 @@
 	$fourth_query = new WP_Query($args); 
 ?>
 
+<!-- Popular -->
 <div class="popular-content">
 	<div class="container">
 		<div class="three columns">
--- section-subheader.php
+++ section-subheader.php
@@ -1,28 +1,28 @@
 <div class="sub-header clearfix">
 
-	<?php if(is_tax('country')) : ?>
+	<?php if(is_tax('country') || is_tax('publisher')) : ?>
 		<div class="choose-filter">
 			<?php
-			$countrys = get_terms('country');
-			$title = '<span class="title">' . __('Choose a country', 'infoamazonia') . '</span>';
-			$current_country = false;
-			if(is_tax('country')) {
-				$current_country = get_query_var('country');
-				$current_country = get_term_by('slug', $country, 'country');
-				$title = '<h1 class="title">' . $current_country->name . '</h1>';
+			$publishers = get_terms('publisher');
+			$title = '<span class="title">' . __('Choose a publisher', 'infocongo') . '</span>';
+			$current_publisher = false;
+			if(is_tax('publisher')) {
+				$current_publisher = get_query_var('publisher');
+				$current_publisher = get_term_by('slug', $publisher, 'publisher');
+				$title = '<h1 class="title">' . $current_publisher->name . '</h1>';
 			}
-			if($countrys) : ?>
+			if($publishers) : ?>
 				<div class="box clearfix">
 						<span class="arrow"></span>
 						<?php echo $title; ?>
 				</div>
 				<ul>
-					<?php if($current_country) : ?>
-						<li class="filter"><a href="<?php echo home_url('/'); ?>" title="<?php _e('All stories', 'infoamazonia'); ?>"><?php _e('All stories', 'infoamazonia'); ?></a></li>
+					<?php if($current_publisher) : ?>
+						<li class="filter"><a href="<?php echo home_url('/'); ?>" title="<?php _e('All stories', 'infocongo'); ?>"><?php _e('All stories', 'infocongo'); ?></a></li>
 					<?php endif; ?>
-					<?php foreach($countrys as $country) : ?>
-						<?php if($current_country && $country->slug == $current_country->slug) continue; ?>
-						<li class="filter"><a href="<?php echo get_term_link($country); ?>" title="<?php echo $country->name; ?>"><?php echo $country->name; ?></a></li>
+					<?php foreach($publishers as $publisher) : ?>
+						<?php if($current_publisher && $publisher->slug == $current_publisher->slug) continue; ?>
+						<li class="filter"><a href="<?php echo get_term_link($publisher); ?>" title="<?php echo $publisher->name; ?>"><?php echo $publisher->name; ?></a></li>
 					<?php endforeach; ?>
 				</ul>
 				<script type="text/javascript">
@@ -37,7 +37,7 @@
 			<?php endif; ?>
 		</div>
 		<div class="toggle-more-filters">
-			<a href="javascript:void(0);"><?php _e('More filters', 'infoamazonia'); ?></a>
+			<a href="javascript:void(0);"><?php _e('More filters', 'infocongo'); ?></a>
 		</div>
 		<div class="clearfix"></div>
 		<div class="more-filters">
