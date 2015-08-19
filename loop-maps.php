681d159b2ac3c45e811546a916bb980563ebc3d4
monoludens <wil.cavalheiro@gmail.com>
2015-08-19 17:01:25 -0300

    loop maps thumb

--- loop-maps.php
+++ loop-maps.php
@@ -3,9 +3,7 @@
 		<li id="post-<?php the_ID(); ?>" <?php post_class('post-item six columns'); ?>>
 			<article class="clearfix">
 				<?php if(has_post_thumbnail()) {
-					echo '<a href="' . get_permalink() .'" title="' . get_the_title() . '">' . get_the_post_thumbnail($post->ID, 'map-thumb') . '</a>';
-				} else {
-					echo '<a href="' . get_permalink() .'" title="' . get_the_title() . '"><img src="' . jeo_get_mapbox_image($post->ID) . '" class="wp-post-image" /></a>';
+					echo '<a href="' . get_permalink() .'" title="' . get_the_title() . '">' . get_the_post_thumbnail($post->ID, 'archive-list') . '</a>';
 				} ?>
 				<header class="post-header">
 					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
