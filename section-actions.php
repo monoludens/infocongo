65553e99e4bcbe33cd68ca1076e56d28eee1caf4
monoludens <wil.cavalheiro@gmail.com>
2015-08-19 12:23:29 -0300

    section actions add

---
+++ section-actions.php
@@ -0,0 +1,19 @@
+<aside id="home-call">
+	<div class="container">
+		<div class="four columns">
+			<span class="lsf"><a class="submit-story" href="#">&#xE041;</a></span>
+			<h3><a href="#" class="submit-story"><?php _e('Submit a story', 'infocongo'); ?></a></h3>
+			<p><a href="#" class="submit-story"><?php _e('Do you have news to share ? Contribute to this map by submitting your story.', 'infocongo'); ?></a></p>
+		</div>
+		<div class="four columns">
+			<span class="lsf"><a href="<?php echo jeo_get_share_url(); ?>">&#xE09c;</a></span>
+			<h3><a href="<?php echo jeo_get_share_url(); ?>"><?php _e('Customize and share', 'infocongo'); ?></a></h3>
+			<p><a href="<?php echo jeo_get_share_url(); ?>"><?php _e('Create a custom map visualization with different news and spread the word.', 'infocongo'); ?></a></p>
+		</div>
+		<div class="four columns">
+			<span class="lsf"><a href="<?php echo infocongo_home_url('/datasets/'); ?>">&#xE115;</a></span>
+			<h3><a href="<?php echo infocongo_home_url('/datasets/'); ?>"><?php _e('Get the data', 'infocongo'); ?></a></h3>
+			<p><a href="<?php echo infocongo_home_url('/datasets/'); ?>"><?php _e('Download all the researched data used to design our maps and help us remix it into new visualizations.', 'infocongo'); ?></a></p>
+		</div>
+	</div>
+</aside>
