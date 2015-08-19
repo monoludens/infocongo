1bfbb66d0ea8d63f4356d320a8c92e9b773ec433
monoludens <wil.cavalheiro@gmail.com>
2015-08-19 17:09:28 -0300

    taxonomies fixes

--- taxonomy-country.php
+++ taxonomy-country.php
@@ -1,25 +1,24 @@
 <?php get_header(); ?>
-
-<section id="content">
-	<div id="map-archive" class="gray-page archive-page">
-		<div class="container">
-		<?php $topics = new WP_Query( array( 'taxonomy' => 'topic')); ?>
-
-						<?php while($topics->have_posts()) : $topic->the_post(); ?>
-
-					<section id="maps" class="map-loop-section archive-list">
-							<div class="twelve columns">
+	<section id="content">
+		<div id="map-archive" class="gray-page archive-page">
+			<div class="container">
+				<section id="maps" class="map-loop-section archive-list">
+					<div class="twelve columns">
 						<header class="page-header">
-								<h1><?php _e('Maps', 'infocongo'); ?></h1>
+						<?php infocongo_taxonomy_filter('Choose a country', 'country'); ?>
+								<div style="width:100%;height:500px;">
+									<?php
+										global $jeo;
+										$jeo->get_map(false, false, true); 
+									?>
+								</div>
+
+
 						</header>
-							</div>
-						<?php get_template_part('loop', 'maps'); ?>
-					</section>
-
-				<?php while; ?>
-			<?php wp_reset_postdata(); ?>
+					</div>
+					<?php get_template_part('loop'); ?>
+				</section>
+			</div>
 		</div>
-	</div>
-</section>
-
+	</section>
 <?php get_footer(); ?>
