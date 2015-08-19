e1ef7f80fe41d4cb3eadfc27f3cada522f01b93a
monoludens <wil.cavalheiro@gmail.com>
2015-08-19 12:20:13 -0300

    submit call add

---
+++ section-submit-call.php
@@ -0,0 +1,10 @@
+<div class="container row">
+	<div class="twelve columns">
+		<aside id="submit-call" class="clearfix">
+			<p><?php _e('Do you have news to share? Contribute to this map by submitting your story. Help broaden the understanding of the global impact of this important region in the world.', 'infocongo'); ?></p>
+			<div class="button-area">
+				<a href="#" class="button submit-story"><?php _e('Submit a story', 'infocongo'); ?></a>
+			</div>
+		</aside>
+	</div>
+</div>
