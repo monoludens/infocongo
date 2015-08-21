7fed4139f7eba17eb284bdfe1e913407324dd922
monoludens <wil.cavalheiro@gmail.com>
2015-08-21 20:58:19 -0300

    archive dataset page positioning improves

--- archive-dataset.php
+++ archive-dataset.php
@@ -4,22 +4,22 @@
 	<div id="map-archive" class="gray-page archive-page">
 		<?php if(have_posts()) : ?>
 
-			<section id="datasets" class="dataset-loop-section archive-list">
+			<section id="datasets" class="dataset-loop-section archive-list container">
 				<div class="page-header">
-					<div class="container">
+					<div class="row">
 						<div class="twelve columns">
-							<h1><?php _e('Datasets', 'infoamazonia'); ?></h1>
+							<h1 class="archive-header"><?php _e('Datasets', 'infocongo'); ?></h1>
 						</div>
 					</div>
 				</div>
 
-				<div class="container">
+				<div class="row">
 					<?php get_template_part('loop', 'dataset'); ?>
 
-					<div class="four columns">
+					<div class="offset-by-one column three columns">
 
 						<div class="row sources sidebar-item">
-							<h3><?php _e('Sources', 'infoamazonia'); ?></h3>
+							<h3><?php _e('Sources', 'infocongo'); ?></h3>
 							<ul>
 								<?php wp_list_categories(array(
 									'taxonomy' => 'source',
@@ -29,7 +29,7 @@
 						</div>
 
 						<div class="row licenses sidebar-item">
-							<h3><?php _e('Licenses', 'infoamazonia'); ?></h3>
+							<h3><?php _e('Licenses', 'infocongo'); ?></h3>
 							<ul>
 								<?php wp_list_categories(array(
 									'taxonomy' => 'license',
@@ -45,6 +45,5 @@
 	</div>
 </section>
 
-<?php get_template_part('section', 'main-widget'); ?>
 
 <?php get_footer(); ?>
