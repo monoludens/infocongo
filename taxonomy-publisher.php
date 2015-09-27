8241666cb2a5b20063671d0ce471d6aec135f42a
monoludens <wil.cavalheiro@gmail.com>
2015-09-27 14:18:39 -0300

    taxonomies country and publisher sync

--- taxonomy-publisher.php
+++ taxonomy-publisher.php
@@ -10,22 +10,31 @@
 					$jeo->get_map(false, false, true); 
 				?>
 			</div>
-			<div class="query-actions">
-							<?php
-							global $wp_query;
-							$args = $wp_query->query;
-							$args = array_merge($args, $_GET);
-							$geojson = jeo_get_api_url($args);
-							$download = jeo_get_api_download_url($args);
-							$rss = add_query_arg(array('feed' => 'rss'));
-							?>
-							<a class="rss" href="<?php echo $rss; ?>"><?php _e('RSS Feed', 'infocongo'); ?></a>
-							<a class="geojson" href="<?php echo $geojson; ?>"><?php _e('Get GeoJSON', 'infocongo'); ?></a>
-							<a class="download" href="<?php echo $download; ?>"><?php _e('Download', 'infocongo'); ?></a>
-						</div>
 			<div class="container">
-				<div class="twelve columns">
-					<?php get_template_part('loop'); ?>
+				<div class="row">
+					<div class="twelve columns">
+						<div class="section-title">
+						<h1>All posts by <?php echo single_term_title(); ?></h1>
+							<div class="query-actions">
+								<?php
+								global $wp_query;
+								$args = $wp_query->query;
+								$args = array_merge($args, $_GET);
+								$geojson = jeo_get_api_url($args);
+								$download = jeo_get_api_download_url($args);
+								$rss = add_query_arg(array('feed' => 'rss'));
+								?>
+								<a class="rss" href="<?php echo $rss; ?>"><?php _e('RSS Feed', 'infocongo'); ?></a>
+								<a class="geojson" href="<?php echo $geojson; ?>"><?php _e('Get GeoJSON', 'infocongo'); ?></a>
+								<a class="download" href="<?php echo $download; ?>"><?php _e('Download', 'infocongo'); ?></a>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="twelve-coluns">
+							<?php get_template_part('loop'); ?>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
