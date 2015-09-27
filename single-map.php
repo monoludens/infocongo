8fcad4a650c1bab180c976f9a3649ddd58bd8c3a
monoludens <wil.cavalheiro@gmail.com>
2015-09-27 14:23:25 -0300

    single map fixes

--- single-map.php
+++ single-map.php
@@ -8,7 +8,7 @@
 	</div>
 </section>
 
-<section id="content">
+
 	<?php
 	$paged = get_query_var('paged') ? get_query_var('paged') : 1;
 	$query = array(
@@ -18,38 +18,43 @@
 	query_posts($query);
 	if(have_posts()) : ?>
 
-        <section id="last-stories" class="loop-section">
-            <div class="section-title">
-                <div class="container">
-                    <div class="twelve columns">
-                        <h3><?php _e('Stories on', 'infoamazonia'); ?> &ldquo;<?php the_title(); ?>&ldquo;</h3>
-                        <div class="query-actions">
-                            <?php
-                            global $wp_query;
-                            $args = $wp_query->query;
-                            $args = array_merge($args, $_GET);
-                            $geojson = jeo_get_api_url($args);
-                            $download = jeo_get_api_download_url($args);
-                            $rss = add_query_arg(array('feed' => 'rss'));
-                            ?>
-                            <a class="rss" href="<?php echo $rss; ?>"><?php _e('RSS Feed', 'infoamazonia'); ?></a>
-                            <a class="geojson" href="<?php echo $geojson; ?>"><?php _e('Get GeoJSON', 'infoamazonia'); ?></a>
-                            <a class="download" href="<?php echo $download; ?>"><?php _e('Download', 'infoamazonia'); ?></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <?php get_template_part('loop'); ?>
-            </div>
-        </section>
+    <div class="container">
+		<div class="row">
+			<div class="twelve columns">
+				<div class="section-title">
+				<h1>Stories on &ldquo;<?php echo the_title(); ?>&ldquo;</h1>
+					<div class="query-actions">
+						<?php
+						global $wp_query;
+						$args = $wp_query->query;
+						$args = array_merge($args, $_GET);
+						$geojson = jeo_get_api_url($args);
+						$download = jeo_get_api_download_url($args);
+						$rss = add_query_arg(array('feed' => 'rss'));
+						?>
+						<a class="rss" href="<?php echo $rss; ?>"><?php _e('RSS Feed', 'infocongo'); ?></a>
+						<a class="geojson" href="<?php echo $geojson; ?>"><?php _e('Get GeoJSON', 'infocongo'); ?></a>
+						<a class="download" href="<?php echo $download; ?>"><?php _e('Download', 'infocongo'); ?></a>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="twelve-coluns">
+					<?php get_template_part('loop'); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+    <div class="container">
+        <?php get_template_part('loop'); ?>
+    </div>
+</section>
 
 	<?php
 	endif;
 	wp_reset_query(); ?>
 
 	<?php get_template_part('section', 'submit-call'); ?>
-</section>
 <ul class="share">
 				<li class="facebook">
 					<div class="fb-like" data-href="<?php the_permalink(); ?>" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-font="verdana" data-action="recommend"></div>
@@ -62,6 +67,5 @@
 				</li>
 			</ul>
 
-<?php get_template_part('section', 'main-widget'); ?>
 
 <?php get_footer(); ?>
