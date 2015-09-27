6a5b59adbc9d1aadb9b1fd54e35d7f577a46edb7
monoludens <wil.cavalheiro@gmail.com>
2015-09-27 14:24:30 -0300

    submit a story fixes

--- inc/submit-story.php
+++ inc/submit-story.php
@@ -10,58 +10,58 @@ function infoamazonia_submit() {
 	<div id="submit-story">
 		<div class="submit-container">
 			<div class="submit-area">
-				<a href="#" class="close-submit-story" title="<?php _e('Close', 'infoamazonia'); ?>">×</a>
-				<h2><?php _e('Submit a story', 'infoamazonia'); ?></h2>
-				<p class="description"><?php _e('Do you have news to share from the Amazon? Contribute to this map by submitting your story. Help broaden the understanding of the global impact of this important region in the world.', 'infoamazonia'); ?></p>
+				<a href="#" class="close-submit-story" title="<?php _e('Close', 'infocongo'); ?>">×</a>
+				<h2><?php _e('Submit a story', 'infocongo'); ?></h2>
+				<p class="description"><?php _e('Do you have news to share from Congo? Contribute to this map by submitting your story. Help broaden the understanding of the global impact of this important region in the world.', 'infocongo'); ?></p>
 				<div class="submit-content">
 					<div class="error"></div>
 					<div class="choice">
 						<div class="story-type">
-							<a href="#" data-choice="submit-story-url" class="button"><?php _e('Submit a url', 'infoamazonia'); ?></a>
-							<a href="#" data-choice="submit-story-full" class="button"><?php _e('Submit full story', 'infoamazonia'); ?></a>
+							<a href="#" data-choice="submit-story-url" class="button"><?php _e('Submit a url', 'infocongo'); ?></a>
+							<a href="#" data-choice="submit-story-full" class="button"><?php _e('Submit full story', 'infocongo'); ?></a>
 						</div>
 					</div>
 					<form id="submit-story-full" class="submit-choice-content">
 						<input type="hidden" name="action" value="infoamazonia_submit" />
 						<p>
-							<label for="story_author_full_name"><?php _e('Your full name', 'infoamazonia'); ?> <span class="required">*</span></label>
+							<label for="story_author_full_name"><?php _e('Your full name', 'infocongo'); ?> <span class="required">*</span></label>
 							<input type="text" name="story[meta][author_name]" id="story_author_full_name" size="30" />
 						</p>
 						<p>
-							<label for="story_author_email"><?php _e('E-mail', 'infoamazonia'); ?> <span class="required">*</span></label>
+							<label for="story_author_email"><?php _e('E-mail', 'infocongo'); ?> <span class="required">*</span></label>
 							<input type="text" name="story[meta][author_email]" id="story_author_email" size="35" />
 						</p>
 						<p>
-							<label for="story_reporter"><?php _e('Reporter', 'infoamazonia'); ?></label>
+							<label for="story_reporter"><?php _e('Reporter', 'infocongo'); ?></label>
 							<input type="text" name="story[meta][reporter]" id="story_reporter" size="30" />
 						</p>
 						<p>
-							<label for="story_title"><?php _e('Story title', 'infoamazonia'); ?> <span class="required">*</span></label>
+							<label for="story_title"><?php _e('Story title', 'infocongo'); ?> <span class="required">*</span></label>
 							<input type="text" name="story[post][post_title]" id="story_title" size="30" />
 						</p>
 						<p>
-							<label for="story_content"><?php _e('Story text', 'infoamazonia'); ?></label>
+							<label for="story_content"><?php _e('Story text', 'infocongo'); ?></label>
 							<textarea name="story[post][post_content]" id="story_content" rows="7" cols="50"></textarea>
 						</p>
 						<p>
-							<label for="story_url"><?php _e('Story url', 'infoamazonia'); ?></label>
+							<label for="story_url"><?php _e('Story url', 'infocongo'); ?></label>
 							<input type="text" name="story[meta][url]" id="story_url" size="60" />
 						</p>
 						<p>
-							<label for="story_picture"><?php _e('Lead picture', 'infoamazonia'); ?></label>
+							<label for="story_picture"><?php _e('Lead picture', 'infocongo'); ?></label>
 							<input type="text" name="story[meta][picture]" id="story_picture" size="60" />
 						</p>
 						<div class="geocode">
 							<p>
-								<label for="story_location"><?php _e('Story location', 'infoamazonia'); ?></label>
+								<label for="story_location"><?php _e('Story location', 'infocongo'); ?></label>
 								<input type="text" name="story[meta][geocode_address]" id="story_location" class="geocoded-address" size="40" />
-								<a class="button open-geocode-box" href="#"><?php _e('Find location on map', 'infoamazonia'); ?></a>
+								<a class="button open-geocode-box" href="#"><?php _e('Find location on map', 'infocongo'); ?></a>
 								<div class="geocode-result" style="display:none;">
 									<input type="hidden" name="story[meta][geocode_latitude]" class="geocoded-latitude" />
 									<input type="hidden" name="story[meta][geocode_longitude]" class="geocoded-longitude" />
 									<p>
-										<?php _e('Latitude:', 'infoamazonia'); ?> <span class="geocoded-latitude"></span><br/>
-										<?php _e('Longitude:', 'infoamazonia'); ?> <span class="geocoded-longitude"></span>
+										<?php _e('Latitude:', 'infocongo'); ?> <span class="geocoded-latitude"></span><br/>
+										<?php _e('Longitude:', 'infocongo'); ?> <span class="geocoded-longitude"></span>
 									</p>
 								</div>
 								<script type="text/javascript">
@@ -70,40 +70,40 @@ function infoamazonia_submit() {
 							</p>
 						</div>
 						<p>
-							<label for="story_date"><?php _e('Publishing date', 'infoamazonia'); ?></label>
+							<label for="story_date"><?php _e('Publishing date', 'infocongo'); ?></label>
 							<input type="text" name="story[meta][publish_date]" id="story_date" size="20" />
 						</p>
 						<p>
-							<label for="story_notes"><?php _e('Notes to the infoamazonia editor', 'infoamazonia'); ?></label>
+							<label for="story_notes"><?php _e('Notes to the infoamazonia editor', 'infocongo'); ?></label>
 							<textarea name="story[meta][notes]" id="story_notes" rows="7" cols="50"></textarea>
 						</p>
-						<input class="button" type="submit" value="<?php _e('Send story', 'infoamazonia'); ?>" />
+						<input class="button" type="submit" value="<?php _e('Send story', 'infocongo'); ?>" />
 					</form>
 					<form id="submit-story-url" class="submit-choice-content">
 						<input type="hidden" name="action" value="infoamazonia_submit" />
 						<p>
-							<label for="story_full_name"><?php _e('Your full name', 'infoamazonia'); ?> <span class="required">*</span></label>
+							<label for="story_full_name"><?php _e('Your full name', 'infocongo'); ?> <span class="required">*</span></label>
 							<input type="text" name="story[meta][author_name]" id="story_full_name" size="30" />
 						</p>
 						<p>
-							<label for="story_email"><?php _e('E-mail', 'infoamazonia'); ?> <span class="required">*</span></label>
+							<label for="story_email"><?php _e('E-mail', 'infocongo'); ?> <span class="required">*</span></label>
 							<input type="text" name="story[meta][author_email]" id="story_email" size="35" />
 						</p>
 						<p>
-							<label for="story_url"><?php _e('Story url', 'infoamazonia'); ?></label>
+							<label for="story_url"><?php _e('Story url', 'infocongo'); ?></label>
 							<input type="text" name="story[meta][url]" id="story_url" size="60" />
 						</p>
 						<div class="geocode">
 							<p>
-								<label for="story_location"><?php _e('Story location', 'infoamazonia'); ?></label>
+								<label for="story_location"><?php _e('Story location', 'infocongo'); ?></label>
 								<input type="text" name="story[meta][geocode_address]" id="story_location" class="geocoded-address" size="40" />
-								<a class="button open-geocode-box" href="#"><?php _e('Find location on map', 'infoamazonia'); ?></a>
+								<a class="button open-geocode-box" href="#"><?php _e('Find location on map', 'infocongo'); ?></a>
 								<div class="geocode-result" style="display:none;">
 									<input type="hidden" name="story[meta][geocode_latitude]" class="geocoded-latitude" />
 									<input type="hidden" name="story[meta][geocode_longitude]" class="geocoded-longitude" />
 									<p>
-										<?php _e('Latitude:', 'infoamazonia'); ?> <span class="geocoded-latitude"></span><br/>
-										<?php _e('Longitude:', 'infoamazonia'); ?> <span class="geocoded-longitude"></span>
+										<?php _e('Latitude:', 'infocongo'); ?> <span class="geocoded-latitude"></span><br/>
+										<?php _e('Longitude:', 'infocongo'); ?> <span class="geocoded-longitude"></span>
 									</p>
 								</div>
 								<script type="text/javascript">
@@ -111,7 +111,7 @@ function infoamazonia_submit() {
 								</script>
 							</p>
 						</div>
-						<input class="button" type="submit" value="<?php _e('Send story', 'infoamazonia'); ?>" />
+						<input class="button" type="submit" value="<?php _e('Send story', 'infocongo'); ?>" />
 					</form>
 				</div>
 			</div>
@@ -132,7 +132,7 @@ function infoamazonia_submit_post() {
 		$post['post_title'] = 'Submission by ' . $story['meta']['author_name'];
 
 	if(!$story['meta']['author_name'] || !$story['meta']['author_email'])
-		return json_death(array('error' => __('Missing information. Please fill all the required fields!', 'infoamazonia')));
+		return json_death(array('error' => __('Missing information. Please fill all the required fields!', 'infocongo')));
 
 	$post_id = wp_insert_post($post);
 	if($post_id) {
@@ -141,7 +141,7 @@ function infoamazonia_submit_post() {
 		}
 		$return['post_id'] = $post_id;
 	} else {
-		$return['error'] = __('Could not save submission', 'infoamazonia');
+		$return['error'] = __('Could not save submission', 'infocongo');
 	}
 
 	header('Content Type: application/json');
